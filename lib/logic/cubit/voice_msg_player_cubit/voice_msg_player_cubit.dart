b708c38b5e4fb05c1540f86e689f149fe12d4f7f
Kasun Dulara <kasundularaam@gmail.com>
2022-02-05 04:00:25 +0530

    voice recoder error fixed

--- lib/logic/cubit/voice_msg_player_cubit/voice_msg_player_cubit.dart
+++ lib/logic/cubit/voice_msg_player_cubit/voice_msg_player_cubit.dart
@@ -1,41 +1,40 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:image/image.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:meta/meta.dart';
 
 part 'voice_msg_player_state.dart';
 
 class VoiceMsgPlayerCubit extends Cubit<VoiceMsgPlayerState> {
-  AudioPlayer player;
-  VoiceMsgPlayerCubit({required this.player}) : super(VoiceMsgPlayerInitial());
+  VoiceMsgPlayerCubit() : super(VoiceMsgPlayerInitial());
+
+  AudioPlayer audioPlayer = AudioPlayer();
 
   Timer? timer;
   int counter = 0;
   Duration timerInterval = const Duration(milliseconds: 200);
 
   Future<void> loadPlayer({required audioFilePath}) async {
-    print("AUDIOOOO>>>>> $audioFilePath");
     try {
       emit(VoiceMsgPlayerLoading());
-      Duration? duration = await player.setFilePath(audioFilePath);
-      print("DURATION>>>>> $duration");
+      Duration? duration = await audioPlayer.setFilePath(audioFilePath);
       if (duration != null) {
         int audioLength = duration.inMilliseconds;
-        await player.setPitch(0);
+        // await audioPlayer.setPitch(0);
         emit(VoiceMsgPlayerLoaded(audioLength: audioLength));
-      }else{
+      } else {
         emit(VoiceMsgPlayerFailed(errorMsg: "An error occurred"));
       }
     } catch (e) {
-      print("ERROR>>>>> $e");
       emit(VoiceMsgPlayerFailed(errorMsg: e.toString()));
     }
   }
 
   Future<void> playPlayer({required int audioLength}) async {
     try {
-      player.play();
+      audioPlayer.play();
       timer = Timer.periodic(timerInterval, (timerVal) async {
         counter = counter + 200;
         if (counter < audioLength) {
@@ -44,7 +43,7 @@ class VoiceMsgPlayerCubit extends Cubit<VoiceMsgPlayerState> {
           timer!.cancel();
           timer = null;
           counter = 0;
-          await player.stop();
+          await audioPlayer.stop();
           emit(VoiceMsgPlayerLoaded(audioLength: audioLength));
         }
       });
@@ -52,5 +51,4 @@ class VoiceMsgPlayerCubit extends Cubit<VoiceMsgPlayerState> {
       emit(VoiceMsgPlayerFailed(errorMsg: e.toString()));
     }
   }
-
 }
